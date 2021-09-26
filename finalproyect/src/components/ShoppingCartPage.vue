bda496b183fcecd537691721f1910b2d0533132b
manaure <manaure23@gmail.com>
2021-09-26 15:30:54 +0200

    se añadieron y modificaron estilos y media querys

--- finalproyect/src/components/ShoppingCartPage.vue
+++ finalproyect/src/components/ShoppingCartPage.vue
@@ -5,14 +5,17 @@
       <section id="cart-description">
         <div class="cart-product">
           <div class="cart-product-img">
-            <img class="img" src="../assets/Icons/zapatillas.svg" alt="" />
+            <img class="img" :src="product.img" alt="" />
           </div>
           <div class="cart-product-description">
-            <h4><strong>Marca</strong></h4>
-            <h4>Modelo articulo</h4>
+            <h4>
+              <strong>{{ product.brand }}</strong>
+            </h4>
+            <h4>{{ product.name }}</h4>
             <h4>Talla:</h4>
+            <h4>Precio: {{ product.price }}</h4>
           </div>
-          <div class="counter" :class="{ active: isActive }">
+          <div class="counter">
             <button class="button-counter" @click="substract">-</button>
             <p>{{ numberArticle }}</p>
             <button class="button-counter" @click="add">+</button>
@@ -23,9 +26,11 @@
         <h3 class="text-cart-price" id="title-total-price">
           <strong>TOTAL</strong>
         </h3>
-        <h4 class="text-cart-price">Subtotal</h4>
+        <h4 class="text-cart-price">
+          Subtotal: {{ (price = product.price * numberArticle) }}
+        </h4>
         <h4 class="text-cart-price">Envio</h4>
-        <h3 class="text-cart-price" id="text-total-price">Total</h3>
+        <h3 class="text-cart-price" id="text-total-price">Total {{ price }}</h3>
         <button class="checkout"><strong>Tramitar pedido</strong></button>
       </section>
     </div>
@@ -33,36 +38,45 @@
 </template>
 
 <script>
+import { womanproducts } from "../assets/Data/Products.json";
+import { manproducts } from "../assets/Data/Products.json";
 export default {
+  props: ["id"],
   data() {
     return {
+      items: [...womanproducts, ...manproducts],
       numberArticle: 1,
+      product: [],
       cartArticles: [],
-      isActive: true,
+      price: 10,
+      totalPrice: 1,
+      size: "",
     };
+  },
+  mounted() {
+    this.product = this.items.find((item) => item.id == this.$route.params.id);
+    this.cartArticles.push(this.product);
+    console.log(this.cartArticles);
+    this.size = this.$route.params.size;
+    console.log(this.size);
   },
   methods: {
     add() {
       this.numberArticle++;
     },
-    ocultar() {
-      this.isActive = !this.isActive;
-    },
     substract() {
-      this.numberArticle--;
-      if (this.numberArticle == 0) {
-        this.ocultar();
-      }
+      if (this.numberArticle === 0) {
+        this.numberArticle = 0;
+      } else this.numberArticle--;
     },
   },
-  //Quitar(id){
-  //  this.article.id = id
-  //  this.cartArticles.splice(id, 1)
-  //}
 };
 </script>
 
-<style>
+<style scoped>
+h4 {
+  line-height: 1.1;
+}
 #shopping-cart-title {
   text-align: left;
   font-size: 40px;
@@ -96,26 +110,26 @@ export default {
 }
 .cart-product-img {
   width: 120px;
-  height: 150px;
+  height: 130px;
   margin-right: 40px;
 }
 .img {
-  width: 100px;
-  height: 100px;
+  width: 130px;
+  height: 130px;
 }
 .ocultar {
   display: none;
 }
 .cart-product-description {
   width: 350px;
-  height: 150px;
+  height: 130px;
   text-align: left;
 }
 .counter {
   display: flex;
   justify-content: space-between;
   width: 120px;
-  height: 150px;
+  height: 130px;
   text-align: center;
   margin-right: 20px;
 }
@@ -158,12 +172,16 @@ export default {
   border-top: 2px solid black;
   padding-top: 10px;
 }
-@media (max-width: 450px) {
+@media (max-width: 376px) {
   #cart-description {
     margin-left: 0px;
   }
+  h4 {
+    font-size: 17px;
+  }
   .cart-product {
-    width: 375px;
+    margin-left: 3px;
+    width: 354px;
     justify-content: space-around;
   }
   .cart-product-img {
@@ -173,7 +191,63 @@ export default {
     margin-left: 10px;
   }
   .img {
+    width: 100px;
+  }
+  .cart-product-description {
+    margin-top: 10px;
+    margin-left: 20px;
+    width: 160px;
+    height: 120px;
+  }
+  .counter {
+    flex-direction: column-reverse;
+    justify-content: space-around;
+    width: 70px;
+    height: 110px;
+    margin: 0px;
+  }
+  .button-counter {
+    margin-top: 5px;
+  }
+  .counter p {
+    text-align: left;
+    margin-top: 5px;
+    margin-bottom: 5px;
+    margin-left: 10px;
+  }
+  #shopping-cart-title {
+    margin-left: 0px;
+    text-align: center;
+  }
+  #cart-price {
+    margin-left: 3px;
+    width: 350px;
+  }
+  .checkout {
+    margin-left: 12px;
+    width: 330px;
+  }
+}
+@media (min-width: 377px) and (max-width: 450px) {
+  #cart-description {
+    margin-left: 0px;
+  }
+  h4 {
+    font-size: 17px;
+  }
+  .cart-product {
+    margin-left: 3px;
+    width: 410px;
+    justify-content: space-around;
+  }
+  .cart-product-img {
     width: 80px;
+    height: 120px;
+    margin-right: 10px;
+    margin-left: 10px;
+  }
+  .img {
+    width: 100px;
   }
   .cart-product-description {
     margin-top: 10px;
@@ -201,8 +275,8 @@ export default {
     text-align: center;
   }
   #cart-price {
-    margin-left: 0px;
-    width: 375px;
+    margin-left: 3px;
+    width: 410px;
   }
   .checkout {
     margin-left: 20px;
@@ -215,10 +289,22 @@ export default {
   }
   #cart-price {
     width: 545px;
+    height: 400px;
   }
   .checkout {
     width: 345px;
     margin-left: 90px;
+  }
+}
+@media (min-width: 1280px) and (max-height: 950px) {
+  #main {
+    height: 545px;
+  }
+}
+@media (min-width: 1900px) and (min-height: 1080px) {
+  #main {
+    height: 670px;
+    justify-content: space-around;
   }
 }
 </style>
