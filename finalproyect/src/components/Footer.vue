0e9513710793225c27e01697f4890db70b50ec80
manaure <manaure23@gmail.com>
2021-09-26 14:44:30 +0200

    no se realizaron cambios en el footer

--- finalproyect/src/components/Footer.vue
+++ finalproyect/src/components/Footer.vue
@@ -1,42 +1,39 @@
 <template>
-  <div>
-    <footer>
-      <section id="social-network">
-        <a href="https://github.com/manaure23" target="_blank"
-          ><img
-            class="icons-social-network"
-            src="../assets/Icons/github.svg"
-            alt=""
-        /></a>
-        <a href="#"
-          ><img
-            class="icons-social-network"
-            src="../assets/Icons/twitter.svg"
-            alt=""
-        /></a>
-        <a href="#"
-          ><img
-            class="icons-social-network"
-            src="../assets/Icons/whatsapp.svg"
-            alt=""
-        /></a>
-        <a href="#"
-          ><img
-            class="icons-social-network"
-            src="../assets/Icons/mail.svg"
-            alt=""
-        /></a>
-      </section>
-      <section id="about-us">
-        <a href="#">colabora</a>
-        <a href="">publicidad</a>
-        <a href="">aviso de privacidad</a>
-        <a href="">acerca de <strong>NOMBRE EMPRESA</strong></a>
-      </section>
-    </footer>
-  </div>
+  <footer>
+    <section id="social-network">
+      <a href="https://github.com/manaure23" target="_blank"
+        ><img
+          class="icons-social-network"
+          src="../assets/Icons/github.svg"
+          alt=""
+      /></a>
+      <a href="#"
+        ><img
+          class="icons-social-network"
+          src="../assets/Icons/twitter.svg"
+          alt=""
+      /></a>
+      <a href="#"
+        ><img
+          class="icons-social-network"
+          src="../assets/Icons/whatsapp.svg"
+          alt=""
+      /></a>
+      <a href="#"
+        ><img
+          class="icons-social-network"
+          src="../assets/Icons/mail.svg"
+          alt=""
+      /></a>
+    </section>
+    <section id="about-us">
+      <a href="#">colabora</a>
+      <a href="">publicidad</a>
+      <a href="">aviso de privacidad</a>
+      <a href="">acerca de <strong>NOMBRE EMPRESA</strong></a>
+    </section>
+  </footer>
 </template>
-
 <script>
 export default {};
 </script>
